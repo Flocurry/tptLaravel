151366d9040484fd79263042724542f793bcac26
Florian <flodarrigand@msn.com>
2018-03-04 18:31:24 +0100

    First commit

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'mail' => 'E-Mail',
+    'psswd' => 'Mot de passe',
+    'name' => 'Nom',
+    'btnLogin' => 'Se connecter',
+    'mdpMessage' => 'Mot de passe oublié'
+
+];
